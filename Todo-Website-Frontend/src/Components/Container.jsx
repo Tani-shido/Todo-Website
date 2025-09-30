a30179df16b202699666eb9b5f18743039b937cc
Tanishq Dehariya <tanishqdehariya1892004@gmail.com>
2025-09-30 20:57:07 +0530

    Create Container.jsx

---
+++ Todo-Website-Frontend/src/Components/Container.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container1 from './Container1'
+
+function Container() {
+  return (
+    <>
+      <div className='border p-4'>
+        <Container1 />
+      </div>
+    </>
+  )
+}
+
+export default Container
