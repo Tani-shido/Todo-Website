a5886aaf10d49188da4a3f8b906576e993ff8574
Tanishq Dehariya <tanishqdehariya1892004@gmail.com>
2025-09-30 20:48:43 +0530

    Update db.js

--- Todo-Website-Backend/db.js
+++ Todo-Website-Backend/db.js
@@ -5,11 +5,11 @@ const ObjectId = mongoose.Types.ObjectId;
 
 const TaskSchema = new Schema({
     Task: {type: String},
+    // --- FIX HERE ---
     isCompleted: {
         type: Boolean,
         default: false
     }
-
 },{timestamps: true}); 
 
 const TaskModel = mongoose.model("Task" , TaskSchema);
